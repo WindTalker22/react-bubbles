cd26ef70cb1df439cd16eab30b96f4c211f7c201
MooseMan <ssaciolo@gmail.com>
2020-01-31 12:08:32 -0500

    Constructed axios request for token,Saved token to localStorage, Built axiosWithAuth modul

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,47 @@
 import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { axiosWithAuth } from "../utils/axiosWithAuth"
 
-const Login = () => {
+const Login = props => {
   // Setting up state
   const [credentials, setCredentials] = useState({ username: "", password: "" })
-  // make a post request to retrieve a token from the api
 
-  // when you have handled the token, navigate to the BubblePage route
+  // Handling Change
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    console.log(credentials)
+  }
+
+  // POST request
+  const login = e => {
+    e.preventDefault()
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        console.log(res, "Hi from the POST then")
+        localStorage.setItem("token", res.data.payload)
+      })
+      .catch(err => {
+        console.log(err, "Hi from the POST err")
+      })
+  }
+
   return (
     <>
-      <form>
-        <input type="text" name="username" valeu="" />
-        <input />
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log In</button>
       </form>
     </>
   )
