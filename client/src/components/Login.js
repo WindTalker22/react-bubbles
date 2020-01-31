03c74a817d5a6d04eef9bd84187afdc61e41344a
MooseMan <ssaciolo@gmail.com>
2020-01-31 11:33:45 -0500

    Built out axiosWithAuth Module

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 
 const Login = () => {
+  // Setting up state
+  const [credentials, setCredentials] = useState({ username: "", password: "" })
   // make a post request to retrieve a token from the api
+
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <input type="text" name="username" valeu="" />
+        <input />
+      </form>
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
